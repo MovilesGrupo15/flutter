2a0e00829bae21c8721566d4b83026265a6ed525
dgomezrey <d.gomezr2@uniandes.edu.co>
2025-04-02 19:31:13 -0500

    Fetch API

--- lib/features/map/data/recycling_point_model.dart
+++ lib/features/map/data/recycling_point_model.dart
@@ -1,10 +1,12 @@
 class RecyclingPoint {
+  final String id;
   final String name;
   final double latitude;
   final double longitude;
   final String description;
 
   RecyclingPoint({
+    required this.id,
     required this.name,
     required this.latitude,
     required this.longitude,
@@ -13,10 +15,11 @@ class RecyclingPoint {
 
   factory RecyclingPoint.fromJson(Map<String, dynamic> json) {
     return RecyclingPoint(
+      id: json['id'],
       name: json['name'],
-      latitude: json['latitude'],
-      longitude: json['longitude'],
-      description: json['description'],
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+      description: json['description'] ?? '',
     );
   }
 }
--- lib/features/map/data/recycling_repository.dart
+++ lib/features/map/data/recycling_repository.dart
@@ -1,20 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'recycling_point_model.dart';
 
 class RecyclingRepository {
-  Future<List<RecyclingPoint>> getRecyclingPoints() async {
-    await Future.delayed(Duration(seconds: 2)); // Simulate API delay
+  // API URL
+  final String _baseUrl = "http://192.168.1.49:8000"; // IP Local si es fisico
 
-    return [
-      RecyclingPoint(
-          name: "Recycling Point 1",
-          latitude: 4.7110,
-          longitude: -74.0721,
-          description: "Plastics and glass accepted."),
-      RecyclingPoint(
-          name: "Recycling Point 2",
-          latitude: 4.7150,
-          longitude: -74.0780,
-          description: "Metal and electronics recycling."),
-    ];
+  Future<List<RecyclingPoint>> getRecyclingPoints() async {
+    try {
+      final response = await http.get(Uri.parse("$_baseUrl/api/points"));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        return data.map((json) => RecyclingPoint.fromJson(json)).toList();
+      } else {
+        throw Exception("Error al obtener los puntos: ${response.statusCode}");
+      }
+    } catch (e) {
+      throw Exception("Error de conexión: $e");
+    }
   }
 }
